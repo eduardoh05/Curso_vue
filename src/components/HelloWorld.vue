ac47498a66c3352d37624106e29a2cfabf9d792c
Eduardo Henrique Alves <ealves@PGE.MS>
2023-12-13 11:25:33 -0400

    Código até a aula 10

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,15 +27,27 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>{{ sourceData.threads }}</div>
   </div>
 </template>
 
 <script>
+
+//importando dados do Json.
+import sourceData from '@/data.json'
+console.log(sourceData)
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      sourceData: sourceData
+    }
   }
+
 }
 </script>
 
---
+++ src/components/PageHome.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>Hello from Page Home</div>
+    <div v-for="thread in threads" :key="thread.id">
+        <h2>{{ thread.title }}</h2>
+        
+        <div v-for="postID in thread.posts" :key = "postID">  
+            <p>{{ userById(postById(postID).userId).name }}</p>
+            <p>{{ postById(postID).text }}</p>
+        
+        </div>
+    
+    </div>
+
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+    data(){
+        return {
+            threads: sourceData.threads,
+            posts: sourceData.posts,
+            users: sourceData.users
+        }
+    },
+    methods:{
+        postById (postId){
+            return this.posts.find(p => p.id === postId)
+        },
+        userById (userId){
+            return this.users.find(u => u.id === userId)
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
