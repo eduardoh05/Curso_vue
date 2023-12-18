1c81477ade8e72ce6f1c5ecbdaee44fb4328fc06
Eduardo Henrique Alves <ealves@PGE.MS>
2023-12-18 11:39:50 -0400

    Código do curso até a aula 18

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,56 +1,22 @@
 <template>
-    <div v-for="thread in threads" 
-        :key="thread.id" 
-        class="col-large push-top">
-        
-      <h1>{{ thread.title }}</h1>
-  
-      <div class="post-list">
-        <div v-for="postID in thread.posts" :key="postID" class="post">
-          <div class="user-info">
-            <a href="#" class="user-name">{{ userById(postById(postID).userId).name }}</a>
-            <a href="#">
-              <img class="avatar-large" :src="userById(postById(postID).userId).avatar" alt="" />
-            </a>
-            <p class="desktop-only text-small">107 posts</p>
-            <p class="desktop-only text-small">23 threads</p>
-            <span class="online desktop-only">online</span>
-          </div>
-  
-          <div class="post-content">
-            <div>
-              <p>{{ postById(postID).text }}</p>
-            </div>
-            <a href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
-          </div>
-  
-          <div class="post-date text-faded">
-            {{ postById(postID).publishedAt }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads = "threads"/>
   </template>
   
   <script>
   import sourceData from '@/data.json'
+  import ThreadList from '@/components/ThreadList.vue'
   
   export default {
+    components: {
+        ThreadList
+    },
+
     data() {
       return {
-        threads: sourceData.threads,
-        posts: sourceData.posts,
-        users: sourceData.users
+        threads: sourceData.threads
       }
     },
-    methods: {
-      postById(postId) {
-        return this.posts.find(p => p.id === postId)
-      },
-      userById(userId) {
-        return this.users.find(u => u.id === userId)
-      }
-    }
   }
   </script>
   
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
 import {createRouter, createWebHistory} from 'vue-router'
+import PageNotFound from '@/components/PageNotFound'
 
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
         name:  'ThreadShow',
         component: PageThreadShow,
         props: true
+    },
+    {
+        path: '/:pathMatch(.*)',
+        name: 'NotFound',
+        component: PageNotFound
     }
 ]
 
